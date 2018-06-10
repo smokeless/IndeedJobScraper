8482c575a086090461676f933bef3f68f7821588
ross <ross.russell.ca@gmail.com>
2018-06-10 15:11:08 -0700

    clean

--- IndeedJob.py
+++
@@ -1,6 +0,0 @@
-class IndeedJob:
-    def __init__(self, title, link, shortDesc, longDesc):
-        self.title = title
-        self.link = link
-        self.shortDesc = shortDesc
-        self.longDesc = longDesc
